5b42b90707ec3d4281d1868c416fd1f48c1c508b
Toufik Hasan <toufikhasan965@gmail.com>
2026-09-19 15:08:10 +0530

    Create main.java

---
+++ 01-BASICS/Variables/main.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Toufik";
+        int age = 21;
+        double cgpa = 8.5;
+        char grade = 'A';
+        boolean student = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + student);
+    }
+}
