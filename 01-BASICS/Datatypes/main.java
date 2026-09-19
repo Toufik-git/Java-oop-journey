1ecf9545e2e93984138deeaefa0eff239c4eb865
Toufik Hasan <toufikhasan965@gmail.com>
2026-09-19 15:09:07 +0530

    Create main.java

---
+++ 01-BASICS/Datatypes/main.java
@@ -0,0 +1,24 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long l = 10000000000L;
+
+        float f = 10.5f;
+        double d = 20.55;
+
+        char c = 'A';
+        boolean flag = true;
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(flag);
+    }
+}
