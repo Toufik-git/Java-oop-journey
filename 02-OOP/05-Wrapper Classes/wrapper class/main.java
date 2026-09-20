80c77d76b08c3b0e36d72168d4fd4db4c61ae031
Toufik Hasan <toufikhasan965@gmail.com>
2026-09-21 02:14:10 +0530

    Create main.java

---
+++ 02-OOP/05-Wrapper Classes/wrapper class/main.java
@@ -0,0 +1,13 @@
+public class IntegerMethods {
+
+    public static void main(String[] args) {
+
+        Integer x = 100;
+
+        System.out.println(x.intValue());
+        System.out.println(x.doubleValue());
+        System.out.println(Integer.max(10, 20));
+        System.out.println(Integer.min(10, 20));
+        System.out.println(Integer.compare(10, 20));
+    }
+}
