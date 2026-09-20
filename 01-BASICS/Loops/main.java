8a9af629409082b1ae67177e294ddb2c3e66608b
Toufik Hasan <toufikhasan965@gmail.com>
2026-09-20 20:54:02 +0530

    Create main.java

---
+++ 01-BASICS/Loops/main.java
@@ -0,0 +1,33 @@
+//for loop
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
+// do while loop
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 10);
+    }
+}
+//While loop
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
