917b08e464724b9c8b2035c89c49c3cce0a7e597
Toufik Hasan <toufikhasan965@gmail.com>
2026-09-21 02:15:02 +0530

    Create main.java

---
+++ 02-OOP/05-Wrapper Classes/Autoboxing/main.java
@@ -0,0 +1,12 @@
+public class AutoboxingExample {
+
+    public static void main(String[] args) {
+
+        int number = 50;
+
+        Integer obj = number;
+
+        System.out.println("Primitive: " + number);
+        System.out.println("Wrapper: " + obj);
+    }
+}
