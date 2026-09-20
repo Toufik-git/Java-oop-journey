fc05944a88a86eef70ae6d54e854d44e21cfbed8
Toufik Hasan <toufikhasan965@gmail.com>
2026-09-21 02:15:40 +0530

    Create main.java

---
+++ 02-OOP/05-Wrapper Classes/Unboxing/main.java
@@ -0,0 +1,12 @@
+public class UnboxingExample {
+
+    public static void main(String[] args) {
+
+        Integer obj = 100;
+
+        int number = obj;
+
+        System.out.println("Wrapper: " + obj);
+        System.out.println("Primitive: " + number);
+    }
+}
